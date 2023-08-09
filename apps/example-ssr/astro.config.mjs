488ca7f2e2a3e61afd91c157a6d972b0c7af64f8
Rune Botten <rbotten@gmail.com>
2023-08-09 10:27:42 -0700

    add ssr example

---
+++ apps/example-ssr/astro.config.mjs
@@ -0,0 +1,15 @@
+import { sanityIntegration } from "@sanity/astro";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    sanityIntegration({
+      projectId: "3do82whm",
+      dataset: "next",
+      // If you are doing static builds you may want opt out of the CDN
+      useCdn: true,
+    }),
+  ],
+  output: 'server'
+});
