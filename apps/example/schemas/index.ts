03b644c3ada4bf79808f72d60ce2f5189d5b4410
narrationsd <narrationsd@gmail.com>
2023-08-22 16:47:39 -0700

    Working entirely nicely, app and Studio, on all four possibilities: no-ssr, ssr, and both examples also on Vercel. Small oversights adjusted, and package added which Vercel didn't apparently notice to peer. Indexed schemas directory configured, as will be needed for real sites.

--- apps/example/schemas/index.ts
+++ apps/example/schemas/index.ts
@@ -1,6 +1,3 @@
-import author from './author'
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
+// import schemas here from folder, and add to schemaTypes array
 
-export const schemaTypes = [ author, blockContent, category, post ]
+export const schemaTypes = [ ]
