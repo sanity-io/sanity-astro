8ceb4e6eada0b294b3d57ef008e0ae0938191e28
Knut Melvær <knut@sanity.io>
2023-09-30 11:26:50 -0700

    fix: use referenece path to include module

--- packages/sanity-astro/types.d.ts
+++ packages/sanity-astro/types.d.ts
@@ -1,5 +1,7 @@
+/// <reference path="./sanity-module.d.ts" />
 import type { ClientConfig } from "@sanity/client";
 
 export type IntegrationOptions = ClientConfig & {
   studioBasePath?: string;
 };
+
