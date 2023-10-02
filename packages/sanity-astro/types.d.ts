fd896afa93e83321b17c1459e23dedf451d92d15
Knut Melvær <knut@sanity.io>
2023-10-02 09:44:13 -0700

    fix: clean up type files

--- packages/sanity-astro/types.d.ts
+++ packages/sanity-astro/types.d.ts
@@ -1,7 +1,5 @@
-/// <reference path="./sanity-module.d.ts" />
 import type { ClientConfig } from "@sanity/client";
 
 export type IntegrationOptions = ClientConfig & {
   studioBasePath?: string;
 };
-
