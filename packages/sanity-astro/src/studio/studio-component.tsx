6fa2fed8082db7c5620d9da5034917357b16ffc2
Christian Grøngaard <christian.groengaard@sanity.io>
2024-05-23 11:14:03 +0200

    fix(deps): update sanity and @sanity/client (#173)
    * chore: clean up studio-component.tsx
    
    * chore: expect TS error
    
    * chore: configure renovate peerDependencies
    
    * fix(deps): update sanity and @sanity/client

--- packages/sanity-astro/src/studio/studio-component.tsx
+++ packages/sanity-astro/src/studio/studio-component.tsx
@@ -3,9 +3,6 @@ import React from "react";
 import { config } from "sanity:studio";
 import { Studio } from "sanity";
 
-// import { useTheme } from "./useTheme";
-//import { usePrefersColorScheme } from "./usePrefersColorScheme";
-
 if (!config) {
   throw new Error(
     "[@sanity/astro]: Can't load Sanity Studio. Check that you've configured it in `sanity.config.js|ts`.",
@@ -13,22 +10,15 @@ if (!config) {
 }
 
 export function StudioComponent() {
-  /* Disabled for now */
-  // const theme = useTheme(config);
-  // const scheme = usePrefersColorScheme();
-
   return (
     <div
-      data-ui="NextStudioLayout"
+      data-ui="AstroStudioLayout"
       style={{
         height: "100vh",
         maxHeight: "100dvh",
         overscrollBehavior: "none",
         WebkitFontSmoothing: "antialiased",
         overflow: "hidden",
-        //fontFamily: theme.fonts.text.family,
-        //backgroundColor:
-        //  theme.color[scheme === "dark" ? "dark" : "light"].default.base.bg,
       }}
     >
       <Studio config={config} />;
