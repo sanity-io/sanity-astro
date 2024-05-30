6cdb0b2e004c9e0fe698d65c4d5f134f14c47903
Christian Grøngaard <christian.groengaard@sanity.io>
2024-05-30 15:00:13 +0200

    chore: add new example app with visual editing enabled (#188)
    The example app uses a new movies Sanity project so we can freely play around
    with the data.

---
+++ apps/movies/src/env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="astro/client" />
+/// <reference types="@sanity/astro/module" />
+
+interface ImportMetaEnv {
+  readonly SANITY_VISUAL_EDITING_ENABLED: string
+  readonly SANITY_API_READ_TOKEN: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
