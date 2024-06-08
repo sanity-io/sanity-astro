0d1e7f27da0265490f8a6430e17043fb9cc405c2
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-06-08 20:11:38 +0200

    chore(movies): prefix env var with PUBLIC_

--- apps/movies/src/env.d.ts
+++ apps/movies/src/env.d.ts
@@ -2,7 +2,7 @@
 /// <reference types="@sanity/astro/module" />
 
 interface ImportMetaEnv {
-  readonly SANITY_VISUAL_EDITING_ENABLED: string
+  readonly PUBLIC_SANITY_VISUAL_EDITING_ENABLED: string
   readonly SANITY_API_READ_TOKEN: string
 }
 
