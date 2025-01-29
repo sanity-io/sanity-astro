9df9988fb2a6bd18279f5e58d63412f3a3ff5428
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-01-29 10:44:47 +0100

    chore: test astro v5 (#269)

--- apps/example-ssr/astro.config.mjs
+++ apps/example-ssr/astro.config.mjs
@@ -1,6 +1,6 @@
 import sanity from '@sanity/astro'
 import {defineConfig} from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 import react from '@astrojs/react'
 
 // https://astro.build/config
