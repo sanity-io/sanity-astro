b1dd9536a6d778f6898e76a438985c414087222f
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-01-31 10:28:29 +0100

    feat: support embedding studios without ssr (#275)
    * feat: support embedding studios without ssr
    
    * Update studio-component.tsx

--- packages/sanity-astro/src/index.ts
+++ packages/sanity-astro/src/index.ts
@@ -1,10 +1,12 @@
 import type {AstroIntegration} from 'astro'
 import {vitePluginSanityClient} from './vite-plugin-sanity-client'
 import {vitePluginSanityStudio} from './vite-plugin-sanity-studio'
+import {vitePluginSanityStudioHashRouter} from './vite-plugin-sanity-studio-hash-router'
 import type {ClientConfig} from '@sanity/client'
 
 type IntegrationOptions = ClientConfig & {
   studioBasePath?: string
+  studioRouterHistory?: 'browser' | 'hash'
 }
 
 const defaultClientConfig: ClientConfig = {
@@ -15,8 +17,10 @@ export default function sanityIntegration(
   integrationConfig: IntegrationOptions = {},
 ): AstroIntegration {
   const studioBasePath = integrationConfig.studioBasePath
+  const studioRouterHistory = integrationConfig.studioRouterHistory === 'hash' ? 'hash' : 'browser'
   const clientConfig = integrationConfig
   delete clientConfig.studioBasePath
+  delete clientConfig.studioRouterHistory
 
   if (!!studioBasePath && studioBasePath.match(/https?:\/\//)) {
     throw new Error(
@@ -36,18 +40,30 @@ export default function sanityIntegration(
                 ...clientConfig,
               }),
               vitePluginSanityStudio({studioBasePath}),
+              vitePluginSanityStudioHashRouter(),
             ],
           },
         })
         // only load this route if `studioBasePath` is set
         if (studioBasePath) {
-          injectRoute({
-            // @ts-expect-error
-            entryPoint: '@sanity/astro/studio/studio-route.astro', // Astro <= 3
-            entrypoint: '@sanity/astro/studio/studio-route.astro', // Astro > 3
-            pattern: `/${studioBasePath}/[...params]`,
-            prerender: false,
-          })
+          // If the studio router history is set to hash, we can load a studio route that doesn't need a server adapter
+          if (studioRouterHistory === 'hash') {
+            injectRoute({
+              // @ts-expect-error
+              entryPoint: '@sanity/astro/studio/studio-route-hash.astro', // Astro <= 3
+              entrypoint: '@sanity/astro/studio/studio-route-hash.astro', // Astro > 3
+              pattern: `/${studioBasePath}`,
+              prerender: true,
+            })
+          } else {
+            injectRoute({
+              // @ts-expect-error
+              entryPoint: '@sanity/astro/studio/studio-route.astro', // Astro <= 3
+              entrypoint: '@sanity/astro/studio/studio-route.astro', // Astro > 3
+              pattern: `/${studioBasePath}/[...params]`,
+              prerender: false,
+            })
+          }
         }
         injectScript(
           'page-ssr',
--- packages/sanity-astro/src/studio/studio-component.tsx
+++ packages/sanity-astro/src/studio/studio-component.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {createHashHistory, type History, type Listener} from 'history'
 // @ts-ignore
 import {config} from 'sanity:studio'
 import {Studio} from 'sanity'
@@ -9,7 +10,11 @@ if (!config) {
   )
 }
 
-export function StudioComponent() {
+export function StudioComponent(props: {history?: 'browser' | 'hash'}) {
+  const history = React.useMemo(
+    () => (props.history === 'hash' ? createHashHistoryForStudio() : undefined),
+    [props.history],
+  )
   return (
     <div
       data-ui="AstroStudioLayout"
@@ -21,7 +26,47 @@ export function StudioComponent() {
         overflow: 'hidden',
       }}
     >
-      <Studio config={config} />
+      <Studio config={config} unstable_history={history} />
     </div>
   )
+}
+
+function createHashHistoryForStudio(): History {
+  const history = createHashHistory()
+  return {
+    get action() {
+      return history.action
+    },
+    get location() {
+      return history.location
+    },
+    get createHref() {
+      return history.createHref
+    },
+    get push() {
+      return history.push
+    },
+    get replace() {
+      return history.replace
+    },
+    get go() {
+      return history.go
+    },
+    get back() {
+      return history.back
+    },
+    get forward() {
+      return history.forward
+    },
+    get block() {
+      return history.block
+    },
+    // Overriding listen to workaround a problem where native history provides history.listen(location => void), but the npm package is history.listen(({action, location}) => void)
+    listen(listener: Listener) {
+      return history.listen(({location}) => {
+        // @ts-expect-error -- working around a bug? in studio
+        listener(location)
+      })
+    },
+  }
 }
